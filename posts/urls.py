417fa6fd8087dbaca2593f8e10ec8d746e26b00e
André <andreccassim@gmail.com>
2025-11-22 20:34:41 -0300

    sem categorias

--- posts/urls.py
+++ posts/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    path('<int:posts_id>/', views.Posts, name='post'),
+    path('<int:posts_id>/', views.detail_posts, name='detail'),
     path('', views.list_posts, name='index'),
     path('search/', views.search_posts, name='search'),
     path('create/', views.create_posts, name='create'),
+    path('update/<int:post_id>/', views.update_posts, name='update'),
+    path('delete/<int:post_id>/', views.delete_posts, name='delete'),
+    path('categorias/', views.list_categorias, name='categorias'),
+    path('categoria/<str:categoria>/', views.posts_by_category, name='categoria_posts'),
 ]
